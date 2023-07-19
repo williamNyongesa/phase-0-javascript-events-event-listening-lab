a7e72752d2547afcc30f3f01db72a735706550a5
williamNyongesa <marakawilliam@gmail.com>
2023-07-19 09:25:50 +0300

    completed lab work

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+// const input = document.getElementById('input');
+
+// input.addEventListener('input', function addingEventListener() {
+//     alert('I was clicked!');
+// });
+
+function addingEventListener(){
+    let input = document.querySelector('#input');
+    input.addEventListener('input',()=> console.log('Input value has been added'))
 }
+addingEventListener();
